f8d42c449ceeddd0317d7df80d73f5f53660365d
Francisco <francisco@eneas.(none)>
2014-09-02 08:23:25 -0300

    Minor changed to polish subroutines.

--- lioamber/dft_get_qm_forces.f
+++ lioamber/dft_get_qm_forces.f
@@ -11,48 +11,49 @@
 ! (a more suitable name for this subroutine would have
 ! been "dft_get_qm_Ederivatives").
 !
-!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+!------------------------------------------------------------------------------!
        USE garcha_mod,ONLY:natom
        IMPLICIT NONE
        REAL*8,INTENT(INOUT)              :: dxyzqm(3,natom)
-       REAL*8,DIMENSION(:,:),ALLOCATABLE :: force_total,force_contrib
+       REAL*8,DIMENSION(:,:),ALLOCATABLE :: force
+       REAL*8,DIMENSION(:,:),ALLOCATABLE :: force1G,forceSG,force3G
        REAL*8                            :: factor
        INTEGER                           :: ii,jj
-!------------------------------------------------------------------------------!
-       allocate(force_total(natom,3),force_contrib(natom,3))
-       open(unit=200,file='forces_check.dat',position="append")
-       force_total=0.0d0
-!      !------------------------------------------------------------!
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
+
+       allocate(force(natom,3))
+       allocate(force1G(natom,3),forceSG(natom,3),force3G(natom,3))
+       open(unit=200,file='forces.dat',position="append")
+       force   = 0.0d0
+       force1G = 0.0d0
+       forceSG = 0.0d0
+       force3G = 0.0d0
+
        call g2g_timer_start('int1G')
-       force_contrib=0.0d0
-       call int1G(force_contrib)
-       force_total=force_total+force_contrib
-!       call print_forceset(200,natom,force_contrib,'F1G:')
+       call int1G(force1G)
        call g2g_timer_stop('int1G')
-!      !------------------------------------------------------------!
        call g2g_timer_start('intSG')
-       force_contrib=0.0d0
-       call intSG(force_contrib)
-       force_total=force_total+force_contrib
-       call print_forceset(200,natom,force_contrib,'FSG:')
+       call intSG(forceSG)
        call g2g_timer_stop('intSG')
-!      !------------------------------------------------------------!
        call g2g_timer_start('int3G')
-       force_contrib=0.0d0
-       call int3G(force_contrib,.true.)
-       force_total=force_total+force_contrib
-!       call print_forceset(200,natom,force_contrib,'F3G:')
+       call int3G(force3G,.true.)
        call g2g_timer_stop('int3G')
-!      !------------------------------------------------------------!
+       force=force1G+forceSG+force3G
+
+!       call print_forceset(200,natom,force1G,'F1G:')
+       call print_forceset(200,natom,forceSG,'FSG:')
+!       call print_forceset(200,natom,force3G,'F3G:')
+
        factor=1.0d0
 c       factor=627.509391D0/0.5291772108D0
        do ii=1,natom;do jj=1,3
-         dxyzqm(jj,ii)=force_total(ii,jj)*factor
+         dxyzqm(jj,ii)=force(ii,jj)*factor
        enddo;enddo
-!      !------------------------------------------------------------!
+
+!------------------------------------------------------------------------------!
        close(200)
-       deallocate(force_total,force_contrib)
-       return;end subroutine dft_get_qm_forces
+       deallocate(force,force1G,forceSG,force3G)
+       return;end subroutine
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
 !
 !
@@ -69,6 +70,7 @@ c       factor=627.509391D0/0.5291772108D0
        enddo
        write(nunit,'(A)')
      > '------------------------------------------------------------'
+!------------------------------------------------------------------------------!
  200   format(1X,A4,1X,I2,3(2X,E15.7),1X)
        return;end subroutine
 !%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%!
