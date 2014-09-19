9998a7cf4c54d16ae349b0a6a9fce3adf5c5ea50
manuelF <m.ferreria@gmail.com>
2014-09-19 10:59:49 -0300

    cambiados ifs unsigned y extraidas cuentas del loop

--- g2g/cuda/kernels/rmm.h
+++ g2g/cuda/kernels/rmm.h
@@ -6,10 +6,10 @@
 // TODO: esto desperdicia la mitad de los threads -> quizas se puede armar una grilla sin los bloques que no hagan nada
 
 template<class scalar_type,bool check_pos>
-__global__ void gpu_update_rmm(const scalar_type* __restrict__ factors, uint points, scalar_type* rmm, const scalar_type* __restrict__ function_values, uint m)
+__global__ void gpu_update_rmm(const scalar_type* __restrict__ factors, int points, scalar_type* rmm, const scalar_type* __restrict__ function_values, int m)
 {
 //    if (blockIdx.x * blockDim.x > blockIdx.y * blockDim.y) return;
-    uint i, j, first_fi, first_fj;
+    int i, j, first_fi, first_fj;
     // There's more than one block; do the math to get position
     if (check_pos) {
         // Figure out where our block is in the lower triangle
@@ -17,10 +17,10 @@ __global__ void gpu_update_rmm(const scalar_type* __restrict__ factors, uint poi
         // solving for j gives determinant 1+8*k_1
         // Thus, 1+8*k_1 must be square of (odd) integer; take sqrt(1+8*k) and get first odd integer below it -
         // get row and column from there
-        uint n = sqrtf(1.0f+8.0f*blockIdx.x);
+        int n = sqrtf(1.0f+8.0f*blockIdx.x);
         n -= (1 - n % 2);
-        uint block_j = (n - 1) / 2;
-        uint block_i = blockIdx.x - (block_j + 1) * block_j / 2;
+        int block_j = (n - 1) / 2;
+        int block_i = blockIdx.x - (block_j + 1) * block_j / 2;
 
         first_fi = block_i*blockDim.x;
         first_fj = block_j*blockDim.y;
@@ -45,9 +45,11 @@ __global__ void gpu_update_rmm(const scalar_type* __restrict__ factors, uint poi
     __shared__ scalar_type functions_j_local[RMM_BLOCK_SIZE_XY][RMM_BLOCK_SIZE_XY+1]; // Fj[point][j]
     __shared__ scalar_type factor_local[RMM_BLOCK_SIZE_XY * RMM_BLOCK_SIZE_XY];			// factor[point]    // TODO: esto seguramente tiene bank conflicts
 
-    uint inc = RMM_BLOCK_SIZE_XY*RMM_BLOCK_SIZE_XY;
-    for (uint point_base = 0; point_base < points; point_base += inc) {
-        uint abs_threadIdx = threadIdx.y * blockDim.x + threadIdx.x;  // absolute threadId inside block
+    int inc = RMM_BLOCK_SIZE_XY*RMM_BLOCK_SIZE_XY;
+    int abs_threadIdx = threadIdx.y * blockDim.x + threadIdx.x;  // absolute threadId inside block
+    bool valid_fi_thread = (first_fi + threadIdx.y) < m;
+    bool valid_fj_thread = (first_fj + threadIdx.y) < m;
+    for (int point_base = 0; point_base < points; point_base += inc) {
 
         __syncthreads();
 
@@ -55,16 +57,14 @@ __global__ void gpu_update_rmm(const scalar_type* __restrict__ factors, uint poi
         if (point_base + abs_threadIdx < points)
             factor_local[abs_threadIdx] = factors[point_base + abs_threadIdx];
 
-        uint last_point = point_base + inc;
+        int last_point = point_base + inc;
 #pragma unroll 16
-        for (uint point = point_base; point < last_point; point += RMM_BLOCK_SIZE_XY) {
+        for (int point = point_base; point < last_point; point += RMM_BLOCK_SIZE_XY) {
             if (point < points) {
                 /* every RMM_BLOCK_SIZE_X iterations, Fi and Fj get filled with RMM_BLOCK_SIZE_Y functions,
                  * for RMM_BLOCK_SIZE_X different points */
                 __syncthreads();
                 bool valid_point = point + threadIdx.x < points;
-                bool valid_fi_thread = (first_fi + threadIdx.y) < m;
-                bool valid_fj_thread = (first_fj + threadIdx.y) < m;
                 bool validFi = valid_point * valid_fi_thread;
                 bool validFj = valid_point * valid_fj_thread;
                 int function_values_fi_index = COALESCED_DIMENSION(points) * (first_fi + threadIdx.y) + (point + threadIdx.x);
@@ -83,7 +83,7 @@ __global__ void gpu_update_rmm(const scalar_type* __restrict__ factors, uint poi
                 functions_j_local[threadIdx.x][threadIdx.y] = validFj * function_values[validFj * function_values_fj_index];
 
                 __syncthreads();
-                for (uint point_sub = 0; point_sub < RMM_BLOCK_SIZE_XY; point_sub++) {
+                for (int point_sub = 0; point_sub < RMM_BLOCK_SIZE_XY; point_sub++) {
                     rmm_local += functions_i_local[point_sub][threadIdx.x] * functions_j_local[point_sub][threadIdx.y];
                 }
             }
