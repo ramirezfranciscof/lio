7232e7bddaa07b516226262eaff16a03c2e9bcea
JuanPM <marcolongo@gmail.com>
2016-11-23 16:48:55 -0300

    agregue FIELD al SCF

--- lioamber/SCF.f
+++ lioamber/SCF.f
@@ -730,13 +730,33 @@ c-------------------------------------------------------
         E1=0.0D0
 c
 c REACTION FIELD CASE --------------------------------------------
-c
+c juanderboy ----------------------------------------------------
+c 
+        IF(FIELD.and.generate_rho0) THEN
+            call dip(ux,uy,uz)
+            write(*,*) 'ux,uy,uz =', ux,uy,uz
+!            fxx=fx
+!            fyy=fy
+!            fzz=fz
+            g=1.0D0
+            factor=2.54D0
+            call intfld(g,Fx,Fy,Fz)
+!           E1=-1.0D0*g*(Fxx*ux+Fyy*uy+Fzz*uz)/fac 
+!           E1=E1-0.50D0*(1.0D0-1.0D0/epsilon)*Qc2/a0
+            E1=-1.00D0*g*(Fx*ux+Fy*uy+Fz*uz)/factor -
+     >      0.50D0*(1.0D0-1.0D0/epsilon)*Qc2/a0
+            do k=1,MM
+               E1=E1+RMM(k)*RMM(M11+k-1)
+            enddo
+        ELSE
+c E1 includes solvent 1 electron contributions
+            do 303 k=1,MM
+ 303             E1=E1+RMM(k)*RMM(M11+k-1)
+        ENDIF
+
         call g2g_timer_start('actualiza rmm')
 c----------------------------------------------------------------
 c E1 includes solvent 1 electron contributions
-        do k=1,MM
-          E1=E1+RMM(k)*RMM(M11+k-1)
-        enddo
         call g2g_timer_sum_pause('Fock integrals')
         call g2g_timer_sum_start('SCF acceleration')
 c
